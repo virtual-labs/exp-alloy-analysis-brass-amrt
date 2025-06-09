a6ebe62c5d5d0ed011eda01f495af20c677409d4
saneeshpfrancis <saneeshpf@gmail.com>
2025-06-09 13:49:00 +0530

    Simulator added

---
+++ experiment/simulation/flash/simulation/simulator.php
@@ -0,0 +1,17 @@
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<div class="post" align="left">&nbsp; <!--leave this space as such.. som wired issue-->
+<?php include('breadcum.php'); ?>
+
+
+  <blockquote> 
+    <span class="title">Alloy_analysis</span>
+    
+	<p>    <div class="postConentPadding">
+	  <?php
+		//@include('vlab/'.$sub.'/'.$brch.'/'.$sim.'/menu.php');
+		@include('menu.php');
+?>
+</p>
+<p align="center">
+  <iframe src="<?php echo 'vlab/'.$sub.'/'.$brch.'/'.$sim.'/'.'index.html'?>" frameborder="0" height="600" width="800" scrolling="no"></iframe></blockquote>&nbsp;
+</div>
